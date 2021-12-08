7df4ce7f3e2168914ff1d71a29fe2be92ed4be2b
Arian Dashti <ariandashti@outlook.com>
2021-12-08 17:31:44 +0330

    Implement Validation Behaviour for application.

---
+++ src/CrudTest.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace CrudTest.Application.Common.Behaviours
+{
+    /**
+     * This pipeline behaviour calls all the provided validators for a request and
+     * calls them all in parallel, then accumulates all their validation failures
+     * into a single one. since the validation are called in parallel care must
+     * be taken that validators are thread safe.
+     */
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            if (!_validators.Any()) return await next();
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults
+                .Where(r => r.Errors.Any())
+                .SelectMany(r => r.Errors)
+                .ToList();
+
+            if (failures.Any())
+                throw new ValidationException(failures);
+
+            return await next();
+        }
+    }
+}
