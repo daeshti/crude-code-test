a9df9825a32468f3be0d1c128e1584ce1b0d5d11
Arian Dashti <ariandashti@outlook.com>
2021-12-08 18:46:46 +0330

    Create get customer with pagination query and dto.

---
+++ src/CrudTest.Application/Customers/Queries/GetCustomersWithPagination/CustomerBriefDto.cs
@@ -0,0 +1,14 @@
+﻿using CrudTest.Application.Common.Mappings;
+using CrudTest.Domain.Entities;
+
+namespace CrudTest.Application.Customers.Queries.GetCustomersWithPagination
+{
+    public class CustomerBriefDto : IMapFrom<Customer>
+    {
+        public int CustomerId { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+    }
+}
---
+++ src/CrudTest.Application/Customers/Queries/GetCustomersWithPagination/GetCustomersWithPaginationQuery.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CrudTest.Application.Common.Interfaces;
+using CrudTest.Application.Common.Mappings;
+using CrudTest.Application.Common.Models;
+using MediatR;
+
+namespace CrudTest.Application.Customers.Queries.GetCustomersWithPagination
+{
+    public class GetCustomersWithPaginationQuery : IRequest<PaginatedList<CustomerBriefDto>>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+
+    
+}
