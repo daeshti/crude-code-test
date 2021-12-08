fd19e01a9ca16a4ee59cc539a09e51f95d96b82e
Arian Dashti <ariandashti@outlook.com>
2021-12-08 17:59:28 +0330

    Create command and event for creating a customer.

---
+++ src/CrudTest.Application/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using CrudTest.Application.Common.Interfaces;
+using CrudTest.Domain.Entities;
+using CrudTest.Domain.Events;
+using MediatR;
+
+namespace CrudTest.Application.Customers.Commands.CreateCustomer
+{
+    /**
+     * A command for creation of <see cref="Customer"/> class.
+     */
+    public class CreateCustomerCommand : IRequest<int>
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+        
+        /**
+         * This optional property will help validate the phone number.
+         */
+        public string PhoneNumberNationality { get; set; }
+
+        public string Email { get; set; }
+        
+        public string BankAccountNumber { get; set; }
+    }
+}
---
+++ src/CrudTest.Domain/Events/CustomerCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using CrudTest.Domain.Common;
+using CrudTest.Domain.Entities;
+
+namespace CrudTest.Domain.Events
+{
+    public class CustomerCreatedEvent : DomainEvent
+    {
+        public CustomerCreatedEvent(Customer customer)
+        {
+            Customer = customer;
+        }
+
+        public Customer Customer { get; }
+    }
+}
