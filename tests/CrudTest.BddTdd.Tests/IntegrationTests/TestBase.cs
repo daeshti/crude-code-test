b319e56bf18d75012681262530e2d3c7acf8a8dd
Arian Dashti <ariandashti@outlook.com>
2021-12-08 21:29:37 +0330

    Creat integration test utility and abstractions.

---
+++ tests/CrudTest.BddTdd.Tests/IntegrationTests/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using static CrudTest.BddTdd.Tests.IntegrationTests.Testing;
+
+namespace CrudTest.BddTdd.Tests.IntegrationTests
+{
+    public class TestBase
+    {
+        [SetUp]
+        public async Task TestSetUp()
+        {
+            await ResetState();
+        }
+    }    
+}
+
+
+
---
+++ tests/CrudTest.BddTdd.Tests/IntegrationTests/Testing.cs
@@ -0,0 +1,117 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using CrudTest.Infrastructure.Persistence;
+using CrudTest.Presentation.Server;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using Respawn;
+
+namespace CrudTest.BddTdd.Tests.IntegrationTests
+{
+    [SetUpFixture]
+    public class Testing
+    {
+        private static IConfigurationRoot _configuration = null!;
+        private static IServiceScopeFactory _scopeFactory = null!;
+        private static Checkpoint _checkpoint = null!;
+        private static string? _currentUserId;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+
+            var startup = new Startup(_configuration);
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
+                w.EnvironmentName == "Development" &&
+                w.ApplicationName == "Helper.WebUI"));
+
+            services.AddLogging();
+
+            startup.ConfigureServices(services);
+
+            _scopeFactory = services.BuildServiceProvider().GetRequiredService<IServiceScopeFactory>();
+
+            _checkpoint = new Checkpoint
+            {
+                TablesToIgnore = new[] {"__EFMigrationsHistory"}
+            };
+
+            EnsureDatabase();
+        }
+
+        private static void EnsureDatabase()
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Database.Migrate();
+        }
+
+        public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+            return await mediator.Send(request);
+        }
+
+        public static async Task ResetState()
+        {
+            await _checkpoint.Reset(_configuration.GetConnectionString("DefaultConnection"));
+
+            _currentUserId = null;
+        }
+
+        public static async Task<TEntity?> FindAsync<TEntity>(params object[] keyValues)
+            where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            return await context.FindAsync<TEntity>(keyValues);
+        }
+
+        public static async Task AddAsync<TEntity>(TEntity entity)
+            where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Add(entity);
+
+            await context.SaveChangesAsync();
+        }
+
+        public static async Task<int> CountAsync<TEntity>() where TEntity : class
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            return await context.Set<TEntity>().CountAsync();
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+        }
+    }
+}
