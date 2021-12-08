c15b965a3fd95c954852c3c5f49f689e73cbf5e1
Arian Dashti <ariandashti@outlook.com>
2021-12-08 17:25:25 +0330

    Create domain event abstractions.

---
+++ src/CrudTest.Domain/Common/DomainEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrudTest.Domain.Common
+{
+    public interface IHasDomainEvent
+    {
+        public List<DomainEvent> DomainEvents { get; set; }
+    }
+
+    public abstract class DomainEvent
+    {
+        public bool IsPublished { get; set; }
+        public DateTimeOffset DateOccurred { get; set; } = DateTime.UtcNow;
+    }
+}
