b6f9953b4322cc3f59755baca01a23f93a4c9d89
Arian Dashti <ariandashti@outlook.com>
2021-12-08 20:15:01 +0330

    Add dependency injection extension methods for infrastructure.

---
+++ src/CrudTest.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿
+using CrudTest.Application.Common.Interfaces;
+using CrudTest.Infrastructure.Persistence;
+using CrudTest.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace CrudTest.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        /**
+         * Adds infrastructure services to the service container.
+         */ 
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseInMemoryDatabase("CrudTestDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("DefaultConnection"),
+                        b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            }
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            services.AddScoped<IDomainEventService, DomainEventService>();
+
+            return services;
+        }
+    }
+}
