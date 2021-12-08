e4b87237c53478e1749b4211a2dc5ee871b6b80b
Arian Dashti <ariandashti@outlook.com>
2021-12-08 17:55:24 +0330

    Implement dependency injection extension methods for application.

---
+++ src/CrudTest.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using CrudTest.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrudTest.Application
+{
+    public static class DependencyInjection
+    {
+        /**
+         * Adds application services to the service container.
+         */
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            services.AddAutoMapper(executingAssembly);
+            services.AddValidatorsFromAssembly(executingAssembly);
+            services.AddMediatR(executingAssembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
