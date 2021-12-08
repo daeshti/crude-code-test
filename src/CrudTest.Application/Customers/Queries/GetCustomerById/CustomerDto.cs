499118cc816cbe2be95af6d3d66b4a5f6780f5be
Arian Dashti <ariandashti@outlook.com>
2021-12-08 18:45:37 +0330

    Implemented get customer by id query.

---
+++ src/CrudTest.Application/Customers/Queries/GetCustomerById/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CrudTest.Application.Common.Mappings;
+using CrudTest.Domain.Entities;
+
+namespace CrudTest.Application.Customers.Queries.GetCustomerById
+{
+    public class CustomerDto : IMapFrom<Customer>
+    {
+        public int CustomerId { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public DateTime DateOfBirth { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string BankAccountNumber { get; set; }
+    }
+}
---
+++ src/CrudTest.Application/Customers/Queries/GetCustomerById/GetCustomerByIdQuery.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CrudTest.Application.Common.Exceptions;
+using CrudTest.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudTest.Application.Customers.Queries.GetCustomerById
+{
+    public class GetCustomerByIdQuery : IRequest<CustomerDto>
+    {
+        public int CustomerId { get; set; }
+    }
+
+    public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, CustomerDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IApplicationDbContext _context;
+
+        public GetCustomerByIdQueryHandler(IMapper mapper, IApplicationDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<CustomerDto> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Customers
+                .Where(c => c.CustomerId == request.CustomerId)
+                .ProjectTo<CustomerDto>(_mapper.ConfigurationProvider)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException();
+            }
+
+            return entity;
+        }
+    }
+}
