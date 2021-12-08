59e2598a3ab93bed0ff5ae45f90d8ed7c8c274c2
Arian Dashti <ariandashti@outlook.com>
2021-12-08 17:47:13 +0330

    Implement common models for application.

---
+++ src/CrudTest.Application/Common/Models/DomainEventNotification.cs
@@ -0,0 +1,19 @@
+﻿using CrudTest.Domain.Common;
+using MediatR;
+
+namespace CrudTest.Application.Common.Models
+{
+    /**
+     * A "Template" for all domain event notifications. Helps creating and maintaining
+     * them easier.
+     */
+    public class DomainEventNotification<TDomainEvent> : INotification where TDomainEvent : DomainEvent
+    {
+        public DomainEventNotification(TDomainEvent domainEvent)
+        {
+            DomainEvent = domainEvent;
+        }
+
+        public TDomainEvent DomainEvent { get; }
+    }
+}
---
+++ src/CrudTest.Application/Common/Models/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrudTest.Application.Common.Models
+{
+    /**
+     * A response type for request with side effects but no info (i.e Create/Delete)
+     */
+    public class Result
+    {
+        internal Result(bool succeeded, IEnumerable<string> errors)
+        {
+            Succeeded = succeeded;
+            Errors = errors.ToArray();
+        }
+
+        public bool Succeeded { get; set; }
+
+        public string[] Errors { get; set; }
+
+        public static Result Success()
+        {
+            return new Result(true, Array.Empty<string>());
+        }
+
+        public static Result Failure(IEnumerable<string> errors)
+        {
+            return new Result(false, errors);
+        }
+    }
+}
