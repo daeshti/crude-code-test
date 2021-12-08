11be73c380a17b2801b3ace989f18634ba13ac7d
Arian Dashti <ariandashti@outlook.com>
2021-12-08 17:43:39 +0330

    Create an common abstractions for application.

---
+++ src/CrudTest.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CrudTest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudTest.Application.Common.Interfaces
+{
+    /**
+     * Since entity framework provides a class instead of an interface this
+     * interface is created to decouple DbContext as a dependency so parts
+     * depending on it can be tested.
+     */
+    public interface IApplicationDbContext
+    {
+        DbSet<Customer> Customers { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/CrudTest.Application/Common/Interfaces/IDomainEventService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using CrudTest.Domain.Common;
+
+namespace CrudTest.Application.Common.Interfaces
+{
+    /**
+     * An interface for domain services, all events are published through this interface
+     * to first, decouple the application from MediatR and second to act as a wrapper.
+     */
+    public interface IDomainEventService
+    {
+        Task Publish(DomainEvent domainEvent);
+    }
+}
