acbadd71867492e7db089a966a74fe664af2cebd
Arian Dashti <ariandashti@outlook.com>
2021-12-08 19:58:01 +0330

    Test and implement entity framework configuration for customer entity.

---
+++ src/CrudTest.Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using CrudTest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CrudTest.Infrastructure.Persistence.Configurations
+{
+    /**
+     * Configures the <see cref="Customer"/> entity for the database.
+     * Optimizes the PhoneNumber property for storage.
+     */
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Ignore(e => e.DomainEvents);
+
+            builder.Property(c => c.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+            
+            builder.Property(c => c.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+            
+            builder.Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(254);
+
+            // Optimized for storage
+            builder.Property(c => c.PhoneNumber)
+                .IsRequired()
+                .HasConversion(str => PhoneNumberAsULong(str),
+                    u64 => ULongAsPhoneNumber(u64));
+
+            builder.Property(c => c.BankAccountNumber)
+                .IsRequired()
+                .HasMaxLength(17);
+        }
+
+        public static ulong PhoneNumberAsULong(string phoneNumber)
+        {
+            var plusRemoved = phoneNumber[1..];
+            var u64 = ulong.Parse(plusRemoved);
+            return u64;
+        }
+
+        public static string ULongAsPhoneNumber(ulong u64)
+        {
+            var plusAdded = $"+{u64}";
+            return plusAdded;
+        }
+    }
+}
---
+++ tests/CrudTest.BddTdd.Tests/UnitTests/Infrastructure/Persistence/Configurations/CustomerConfigurationTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+using CrudTest.Infrastructure.Persistence.Configurations;
+using FluentAssertions;
+using Xunit;
+
+namespace CrudTest.BddTdd.Tests.UnitTests.Infrastructure.Persistence.Configurations
+{
+    
+    public class CustomerConfigurationTests
+    {
+
+        [Theory]
+        [InlineData("+989029058590", 989029058590)]
+        public void PhoneNumber_ShouldMapToUlong(string phoneNumber, ulong u64)
+        {
+            var result = CustomerConfiguration.PhoneNumberAsULong(phoneNumber);
+            result.Should().Be(u64);
+        }
+
+        [Theory]
+        [InlineData(989029058590, "+989029058590")]
+        public void ULong_ShouldMapToPhoneNumber(ulong u64, string phoneNumber)
+        {
+            var result = CustomerConfiguration.ULongAsPhoneNumber(u64);
+            result.Should().Be(phoneNumber);
+        }
+        
+    }
+}
