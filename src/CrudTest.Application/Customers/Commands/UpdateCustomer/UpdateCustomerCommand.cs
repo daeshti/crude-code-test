a925ae594fe6bc217e4dd4dec6697353f4e47018
Arian Dashti <ariandashti@outlook.com>
2021-12-08 19:12:59 +0330

    Implement a handler for update customer command.

--- src/CrudTest.Application/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
+++ src/CrudTest.Application/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -8,6 +8,9 @@ using MediatR;
 
 namespace CrudTest.Application.Customers.Commands.UpdateCustomer
 {
+    /**
+     * A command for update of <see cref="Customer"/> class.
+     */
     public class UpdateCustomerCommand : IRequest
     {
         public int CustomerId { get; set; }
@@ -28,5 +31,37 @@ namespace CrudTest.Application.Customers.Commands.UpdateCustomer
         public string Email { get; set; }
         
         public string BankAccountNumber { get; set; }
+    }
+    
+    public class UpdateCustomerCommandHandler : IRequestHandler<UpdateCustomerCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateCustomerCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Customers
+                .FindAsync(new object[] { request.CustomerId }, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Customer), request.CustomerId);
+            }
+
+            entity.FirstName = request.FirstName;
+            entity.LastName = request.LastName;
+            entity.DateOfBirth = request.DateOfBirth;
+            entity.PhoneNumber = request.PhoneNumber;
+            entity.Email = request.Email;
+            entity.BankAccountNumber = request.BankAccountNumber;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
     }
 }
