c054807a406fd087f3c51399f82168c560e82030
Arian Dashti <ariandashti@outlook.com>
2021-12-08 20:34:15 +0330

    Create an API Controller base class.

---
+++ src/CrudTest.Presentation.Server/Controllers/ApiControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrudTest.Presentation.Server.Controllers
+{
+    /**
+     * This base class extends ControllerBase with CQRS capabilities we need.
+     */
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private ISender _mediator = null!;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
