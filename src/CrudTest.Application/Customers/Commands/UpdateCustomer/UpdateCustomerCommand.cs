3ff51abd123dcb3f9af9269a07806dfbfeecefe3
Arian Dashti <ariandashti@outlook.com>
2021-12-08 19:02:16 +0330

    Create update customer command.

---
+++ src/CrudTest.Application/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CrudTest.Application.Common.Exceptions;
+using CrudTest.Application.Common.Interfaces;
+using CrudTest.Domain.Entities;
+using MediatR;
+
+namespace CrudTest.Application.Customers.Commands.UpdateCustomer
+{
+    public class UpdateCustomerCommand : IRequest
+    {
+        public int CustomerId { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+        
+        /**
+         * This optional property will help validate the phone number.
+         */
+        public string PhoneNumberNationality { get; set; }
+
+        public string Email { get; set; }
+        
+        public string BankAccountNumber { get; set; }
+    }
+}
