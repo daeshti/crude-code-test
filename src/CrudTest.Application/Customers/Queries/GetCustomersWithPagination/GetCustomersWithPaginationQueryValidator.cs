0b00c7ca54d82a6db469f9b6b276abd895fa3c3a
Arian Dashti <ariandashti@outlook.com>
2021-12-08 18:57:58 +0330

    Test and implement validator for get customers with pagination query.

---
+++ src/CrudTest.Application/Customers/Queries/GetCustomersWithPagination/GetCustomersWithPaginationQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace CrudTest.Application.Customers.Queries.GetCustomersWithPagination
+{
+    public class GetCustomersWithPaginationQueryValidator : AbstractValidator<GetCustomersWithPaginationQuery>
+    {
+        public GetCustomersWithPaginationQueryValidator()
+        {
+            RuleFor(x => x.PageNumber)
+                .GreaterThanOrEqualTo(1).WithMessage("'Page Number' at least greater than or equal to 1.");
+
+            RuleFor(x => x.PageSize)
+                .GreaterThanOrEqualTo(1).WithMessage("'Page Size' at least greater than or equal to 1.");
+        }
+    }
+}
---
+++ tests/CrudTest.BddTdd.Tests/UnitTests/Application/Customers/Queries/GetCustomersWithPagination/GetCustomersWithPaginationQueryValidatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CrudTest.Application.Customers.Queries.GetCustomersWithPagination;
+using FluentAssertions;
+using Xunit;
+
+namespace CrudTest.BddTdd.Tests.UnitTests.Application.Customers.Queries.GetCustomersWithPagination
+{
+    public class GetCustomersWithPaginationQueryValidatorTests
+    {
+        private readonly GetCustomersWithPaginationQueryValidator _validator;
+
+        public GetCustomersWithPaginationQueryValidatorTests()
+        {
+            _validator = new GetCustomersWithPaginationQueryValidator();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public async Task PageNumberLessThanOne_ShouldFail(int pageNumber)
+        {
+            var validationResult = await _validator.ValidateAsync(new GetCustomersWithPaginationQuery
+            {
+                PageNumber = pageNumber,
+                PageSize = 100
+            });
+
+            validationResult.Errors.Select(e => e.ErrorMessage).Should().BeEquivalentTo(new[]
+            {
+                "'Page Number' at least greater than or equal to 1."
+            });
+        }
+        
+        
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public async Task PageSizeLessThanOne_ShouldFail(int pageSize)
+        {
+            var validationResult = await _validator.ValidateAsync(new GetCustomersWithPaginationQuery
+            {
+                PageNumber = 100,
+                PageSize = pageSize
+            });
+
+            validationResult.Errors.Select(e => e.ErrorMessage).Should().BeEquivalentTo(new[]
+            {
+                "'Page Size' at least greater than or equal to 1."
+            });
+        }
+
+        [Fact]
+        public async Task PageNumberAndPageSizeGreaterThanZero_ShouldPass()
+        {
+            var validationResult = await _validator.ValidateAsync(new GetCustomersWithPaginationQuery
+            {
+                PageNumber = 100,
+                PageSize = 100
+            });
+
+            validationResult.IsValid.Should().BeTrue();
+        }
+    }
+}
